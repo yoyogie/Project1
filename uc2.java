c3dc5886120852d5c42f17bf4966efa1277c4f52
kartik <co.kartikb@gmail.com>
2026-03-02 12:51:05 +0530

    uc2

---
+++ uc2.java
@@ -0,0 +1,13 @@
+public class uc2 {
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******    ****** ");
+        System.out.println("*     *  *     *   *     *   *     *");
+        System.out.println("*     *  *     *   *     *   *     *");
+        System.out.println("*     *  *     *   ******    ****** ");
+        System.out.println("*     *  *     *   *         *      ");
+        System.out.println("*     *  *     *   *         *      ");
+        System.out.println(" *****    *****    *         *      ");
+
+    }
+}
